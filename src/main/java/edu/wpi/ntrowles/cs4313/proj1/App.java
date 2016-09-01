a1189be72ed6397616f802bd11bb570c5475851b
Nicholas Rowles <cpufourproject@gmail.com>
2016-08-31 20:58:07 -0400

    Initial Commit

---
+++ src/main/java/edu/wpi/ntrowles/cs4313/proj1/App.java
@@ -0,0 +1,13 @@
+package edu.wpi.ntrowles.cs4313.proj1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
