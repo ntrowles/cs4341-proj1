db9c4431ebb3864b980741f67bb6b840b4551ec7
Nicholas Rowles <cpufourproject@gmail.com>
2016-09-02 15:30:22 -0400

    Started work on greedy search

--- src/main/java/edu/wpi/ntrowles/cs4313/proj1/GreedySearch.java
+++ src/main/java/edu/wpi/ntrowles/cs4313/proj1/GreedySearch.java
@@ -3,8 +3,11 @@ package edu.wpi.ntrowles.cs4313.proj1;
 public class GreedySearch implements Search {
 
 	public Solution search(Problem moProblems) {
-		// TODO Auto-generated method stub
+		double startState = moProblems.getStartNum();
+		Node root = new Node(startState, null, '0', 0, 0);
+
 		return null;
+		//Solution solution = (path, calcNum, )
 	}
 
 }
