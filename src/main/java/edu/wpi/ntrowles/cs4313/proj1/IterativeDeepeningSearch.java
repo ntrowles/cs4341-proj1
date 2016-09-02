bc4be0bde1d5b2f3368a875ad403c76418442d58
bgsarkis <bgsarkis@gmail.com>
2016-09-02 15:46:40 -0400

    Rebasing, pseudocoding the Iterative Deepening Search.

--- src/main/java/edu/wpi/ntrowles/cs4313/proj1/IterativeDeepeningSearch.java
+++ src/main/java/edu/wpi/ntrowles/cs4313/proj1/IterativeDeepeningSearch.java
@@ -10,7 +10,15 @@ package edu.wpi.ntrowles.cs4313.proj1;
 public class IterativeDeepeningSearch implements Search {
 
 	public Solution search(Problem moProblems) {
-		for(moProblems.getStartNum())
+		int depth = 0;
+		//for depth = 0 to infinity do
+		//Result assigned Depth-Limited-Search (problem, depth)
+		//If result != cutoff then return result
+		return null;
+	}
+	
+	private Solution DepthLimitedSearch (Problem problem, int limit){
+		//return recursive DLS (Make-Node(problem
 		return null;
 	}
 
