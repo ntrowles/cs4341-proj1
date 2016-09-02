b1704e923a0e7373cc733e86988ac297b12030e1
Nicholas Rowles <cpufourproject@gmail.com>
2016-09-02 13:48:33 -0400

    Added fields to Problem and Solution objects

--- src/main/java/edu/wpi/ntrowles/cs4313/proj1/Problem.java
+++ src/main/java/edu/wpi/ntrowles/cs4313/proj1/Problem.java
@@ -1,5 +1,10 @@
 package edu.wpi.ntrowles.cs4313.proj1;
 
+import java.util.List;
+
 public class Problem {
-	
+	private double startNum;
+	private double endNum;
+	private double maxTime;
+	private List<String> operators;
 }
--- src/main/java/edu/wpi/ntrowles/cs4313/proj1/Solution.java
+++ src/main/java/edu/wpi/ntrowles/cs4313/proj1/Solution.java
@@ -3,6 +3,13 @@ package edu.wpi.ntrowles.cs4313.proj1;
 import java.util.List;
 
 public class Solution {
-	private List<String> path;
+	private List<String> path;   //path of solution
+	private double calcNum;      //calculated number after path
+	private double startNum;
+	private double endNum;
+	
+	private double timeToExec;
+	private int nodesExpanded;
+	private int maxSearchDepth;
 	
 }
