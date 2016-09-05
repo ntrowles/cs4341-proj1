7b3f724f20376de5c4a7d6dd66e7f7078c6d3ba3
Nicholas Rowles <cpufourproject@gmail.com>
2016-09-05 18:48:06 -0400

    Added a test for iterative deepening search

---
+++ src/test/java/edu/wpi/ntrowles/cs4313/proj1/IterativeDeepeningSearchTest.java
@@ -0,0 +1,11 @@
+package edu.wpi.ntrowles.cs4313.proj1;
+
+import org.junit.Test;
+
+public class IterativeDeepeningSearchTest {
+	@Test
+	public void searchTest1(){
+		String[] args = {"src/test/resources/test_3"};
+		Main.main(args);
+	}
+}
