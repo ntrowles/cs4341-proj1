e7494254535ba7588febb15d43121bcd226c4932
Nicholas Rowles <cpufourproject@gmail.com>
2016-09-13 19:09:00 -0400

    Added map for probabilities

--- src/main/java/edu/wpi/ntrowles/cs4313/proj2/genetic/GeneticAlgorithmSearch.java
+++ src/main/java/edu/wpi/ntrowles/cs4313/proj2/genetic/GeneticAlgorithmSearch.java
@@ -1,6 +1,8 @@
 package edu.wpi.ntrowles.cs4313.proj2.genetic;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import edu.wpi.ntrowles.cs4313.proj1.beans.Problem;
 import edu.wpi.ntrowles.cs4313.proj1.beans.Solution;
@@ -32,30 +34,72 @@ public class GeneticAlgorithmSearch implements Search {
 	}
 	
 	public GeneticSolutionInfo geneticAlgorithmSearch(Problem prob, Fitness fit, ArrayList<Solution> initPop, long initTimeMillis){
+		final double timeBuffer = 0.05;
 		
 		//TODO create new genetic algorithm info object
 		ArrayList<Solution> population = new ArrayList<Solution>();
 		population.addAll(initPop);
 		
-		while(System.currentTimeMillis()/1000.0 < (initTimeMillis/1000.0 + prob.getMaxTime())){
+		while(System.currentTimeMillis()/1000.0 < (initTimeMillis/1000.0 + prob.getMaxTime() - timeBuffer)){
 			//check if any solution is correct
+			for(Solution solution : population){
+				if(fit.evaluateFitness(solution, prob) == 0){
+					return generateGeneticSolutionInfo(solution);
+				}
+			}
 			
-			
+			//create new population
 			ArrayList<Solution> newPop = new ArrayList<Solution>();
-			Solution x = randomSelection(prob, fit, population);
-			Solution y = randomSelection(prob, fit, population);
 			
-			//Solution child
+			//create map
+			Map<ArrayList<Solution>, Double> probMap = generateProbabilities(prob, fit, population);
 			
+			for(int i=0; i<population.size(); i++){
+				//randomly select two children
+				Solution x = randomSelection(probMap, population);
+				Solution y = randomSelection(probMap, population);
+				
+				//breed child
+				Solution child = reproduce(x,y);
+				
+				//mutate child
+				child = mutate(child);
+				
+				//add child to new population
+				newPop.add(child);
+			}
+			
+			//assign new population to population
+			population = newPop;
 		}
 		
+		Solution bestSol = getBestSolution(population);
 		
 		
 		//FIXME return genetic algorithm info object
 		return null;
 	}
 	
-	public Solution randomSelection(Problem prob, Fitness fit, ArrayList<Solution> population){
+	private Solution getBestSolution(ArrayList<Solution> population) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	private Map<ArrayList<Solution>, Double> generateProbabilities(Problem prob, Fitness fit,
+			ArrayList<Solution> population) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	private GeneticSolutionInfo generateGeneticSolutionInfo(Solution solution) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public Solution randomSelection(Map<ArrayList<Solution>, Double> probMap, ArrayList<Solution> population){
 		//TODO finish
 		return null;
 	}
