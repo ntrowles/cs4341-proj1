9961d32b33d6121b251d0401ffcca4cf8c04f136
Nicholas Rowles <cpufourproject@gmail.com>
2016-09-13 17:29:14 -0400

    Starting project 2

---
+++ src/main/java/edu/wpi/ntrowles/cs4313/proj2/Main.java
@@ -0,0 +1,10 @@
+package edu.wpi.ntrowles.cs4313.proj2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/edu/wpi/ntrowles/cs4313/proj2/genetic/GeneticAlgorithmSearch.java
@@ -0,0 +1,14 @@
+package edu.wpi.ntrowles.cs4313.proj2.genetic;
+
+import edu.wpi.ntrowles.cs4313.proj1.beans.Problem;
+import edu.wpi.ntrowles.cs4313.proj1.beans.SolutionInfo;
+import edu.wpi.ntrowles.cs4313.proj1.utils.Search;
+
+public class GeneticAlgorithmSearch implements Search {
+
+	public SolutionInfo search(Problem moProblems) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
