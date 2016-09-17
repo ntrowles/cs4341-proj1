73fdae9182bdbe9a4f1a4a8b59ebc7d404be22e4
bgsarkis <bgsarkis@gmail.com>
2016-09-17 15:31:50 -0400

    Used Project 2 main instead of Project one.  Genetic algorithm is recognized.

--- src/test/java/edu/wpi/ntrowles/cs4313/proj2/GeneticAlgorithmSearchTest.java
+++ src/test/java/edu/wpi/ntrowles/cs4313/proj2/GeneticAlgorithmSearchTest.java
@@ -2,12 +2,12 @@ package edu.wpi.ntrowles.cs4313.proj2;
 
 import org.junit.Test;
 
-import edu.wpi.ntrowles.cs4313.proj1.Main;
+import edu.wpi.ntrowles.cs4313.proj2.Main;
 
 public class GeneticAlgorithmSearchTest {
 	@Test
 	public void searchTest1(){
-		String[] args = {"src/test/resources/greedy_1"};
+		String[] args = {"src/test/resources/genetic_1"};
 		Main.main(args);
 	}
 }
