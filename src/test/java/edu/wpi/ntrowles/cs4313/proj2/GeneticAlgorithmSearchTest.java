6bd78b89b9baaecdc39da7f2ca4ac09eba8091ae
bgsarkis <bgsarkis@gmail.com>
2016-09-19 19:16:24 -0400

    .

--- src/test/java/edu/wpi/ntrowles/cs4313/proj2/GeneticAlgorithmSearchTest.java
+++ src/test/java/edu/wpi/ntrowles/cs4313/proj2/GeneticAlgorithmSearchTest.java
@@ -11,19 +11,19 @@ public class GeneticAlgorithmSearchTest {
 		Main.main(args);
 	}
 	public void searchTest2(){
-		String[] args = {"src/test/resources/genetic_2"};
-		Main.main(args);
+		String[] args2 = {"src/test/resources/genetic_2"};
+		Main.main(args2);
 	}
 	public void searchTest3(){
-		String[] args = {"src/test/resources/genetic_3"};
-		Main.main(args);
+		String[] args3 = {"src/test/resources/genetic_3"};
+		Main.main(args3);
 	}
 	public void searchTest4(){
-		String[] args = {"src/test/resources/genetic_4"};
-		Main.main(args);
+		String[] args4 = {"src/test/resources/genetic_4"};
+		Main.main(args4);
 	}
 	public void searchTest5(){
-		String[] args = {"src/test/resources/genetic_5"};
-		Main.main(args);
+		String[] args5 = {"src/test/resources/genetic_5"};
+		Main.main(args5);
 	}
 }
