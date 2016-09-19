ede69baf01671440533f55a580169a65d4e7ab9c
bgsarkis <bgsarkis@gmail.com>
2016-09-19 19:47:57 -0400

    again

--- src/test/java/edu/wpi/ntrowles/cs4313/proj2/GeneticAlgorithmSearchTest.java
+++ src/test/java/edu/wpi/ntrowles/cs4313/proj2/GeneticAlgorithmSearchTest.java
@@ -5,26 +5,31 @@ import org.junit.Test;
 import edu.wpi.ntrowles.cs4313.proj2.Main;
 
 public class GeneticAlgorithmSearchTest {
+	
 	@Test
 	public void searchTest1(){
 		String[] args = {"src/test/resources/genetic_1"};
 		Main.main(args);
 	}
+	
 	@Test
 	public void searchTest2(){
 		String[] args = {"src/test/resources/genetic_2"};
 		Main.main(args);
 	}
+	
 	@Test
 	public void searchTest3(){
 		String[] args = {"src/test/resources/genetic_3"};
 		Main.main(args);
 	}
+	
 	@Test
 	public void searchTest4(){
 		String[] args = {"src/test/resources/genetic_4"};
 		Main.main(args);
 	}
+	
 	@Test
 	public void searchTest5(){
 		String[] args = {"src/test/resources/genetic_5"};
