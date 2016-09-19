65090b841b6fc1d2ad7c509f4c03a5ef90086981
bgsarkis <bgsarkis@gmail.com>
2016-09-19 18:51:41 -0400

    Documentation and another test case.

--- src/main/java/edu/wpi/ntrowles/cs4313/proj2/utils/DifferenceFitness.java
+++ src/main/java/edu/wpi/ntrowles/cs4313/proj2/utils/DifferenceFitness.java
@@ -5,6 +5,12 @@ import edu.wpi.ntrowles.cs4313.proj1.beans.Solution;
 
 public class DifferenceFitness implements Fitness {
 
+	/**
+	 * The heuristic function modeled as a fitness function.
+	 * @param sol Solution being evaluated.
+	 * @param prob Used for the goal number
+	 * @return Closeness to goal
+	 */
 	public double evaluateFitness(Solution sol, Problem prob) {
 		return Math.abs(sol.getEndNum() - prob.getGoalNum());
 	}
