8036b291c47d4e22f379a4d6db2572e4f30abddb
Nicholas Rowles <cpufourproject@gmail.com>
2016-09-19 22:02:53 -0400

    Outline for testing experimentation

--- src/test/java/edu/wpi/ntrowles/cs4313/proj2/ExperimentsTest.java
+++ src/test/java/edu/wpi/ntrowles/cs4313/proj2/ExperimentsTest.java
@@ -1,12 +1,17 @@
 package edu.wpi.ntrowles.cs4313.proj2;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 
 import edu.wpi.ntrowles.cs4313.proj1.beans.Problem;
+import edu.wpi.ntrowles.cs4313.proj1.beans.Solution;
 import edu.wpi.ntrowles.cs4313.proj1.utils.ProblemParser;
 import edu.wpi.ntrowles.cs4313.proj2.genetic.GeneticAlgorithmSearch;
+import edu.wpi.ntrowles.cs4313.proj2.utils.DifferenceFitness;
 
 public class ExperimentsTest {
 	@Test
@@ -22,13 +27,23 @@ public class ExperimentsTest {
 		}
 		
 		double[] times = {0.25, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 60};
-		double[][] timedResults = new double[10][14];
-		GeneticAlgorithmSearch search = new GeneticAlgorithmSearch(20);
+		double[] summedResults = new double[14];
+		GeneticAlgorithmSearch searcher = new GeneticAlgorithmSearch(20);
 		
 		//run this 10 times
 		for(int i=0; i<10; i++){
-			//double[i] = search.search(problem);
+			Map<Double, Solution> timedSol = searcher.geneticAlgorithmSearch(problem, new DifferenceFitness(), times);
+			for(int j=0; j<times.length; j++){
+				summedResults[j] += Math.abs(timedSol.get(times[j]).getEndNum() - problem.getGoalNum());
+			}
+		}
+		
+		Map<Double, Double> graph = new HashMap<Double, Double>();
+		for(int i=0; i<times.length; i++){
+			graph.put(times[i], summedResults[i]/10);
+			System.out.println("Time: " + times[i] + " - Average Error: " + summedResults[i]/10);
 		}
 
+		
 	}
 }
