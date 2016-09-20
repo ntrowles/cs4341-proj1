3f0d7b04fe7f4da64d763eecd7bce727676a66f8
Nicholas Rowles <cpufourproject@gmail.com>
2016-09-19 20:58:47 -0400

    Created new genetic test for the experiment

--- src/test/java/edu/wpi/ntrowles/cs4313/proj2/ExperimentsTest.java
+++ src/test/java/edu/wpi/ntrowles/cs4313/proj2/ExperimentsTest.java
@@ -1,5 +1,12 @@
 package edu.wpi.ntrowles.cs4313.proj2;
 
-public class ExperimentsTest {
+import org.junit.Test;
 
+public class ExperimentsTest {
+	@Test
+	public void experimentTest(){
+		double[][] timedResults = new double[10][14];
+		String[] args = {"src/test/resources/genetic_0"};
+		Main.main(args);
+	}
 }
